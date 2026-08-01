9428478a0e4407586862fe313b97ca3ae80302f4
Сысоев Никита <nsisoev1990@gmail.com>
2026-08-01 21:48:25 +0200

    Добавил все проекты и папки

---
+++ opinions_app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SECRET_KEY'] = 'SECRET KEY'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Импортируем остальные модули, чтобы зарегистрировать маршруты, обработчики и команды
+from . import models, views, error_handler, cli_commands
